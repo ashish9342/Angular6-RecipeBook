508189e9c45a62cc435b1b726ecac031082b9674
Ashish Rawat <ashish.rawat@lighting.com>
2018-06-01 11:10:26 +0530

    Upadted Recipe component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Recipe 1', 'decription','./assets/food.jpg'),
+    new Recipe('Recipe 2', 'decription','./assets/food.jpg'),
+    new Recipe('Recipe 3', 'decription','./assets/food.jpg')
+
+  ];
 
   constructor() { }
 
