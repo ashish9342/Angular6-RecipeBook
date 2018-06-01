cbb9f2c978fb1cc7fed9b2919281bde68a2c005a
Ashish Rawat <ashish.rawat@lighting.com>
2018-06-01 11:48:40 +0530

    creating Ingredient model in shared folder

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,12 @@
+export class Ingredient{
+  // public name : string;
+  // public amount : number;
+  //
+  // constructor(name:string, amount: number){
+  //   this.name = name;
+  //   this.amount = amount;
+  // }
+
+  // shortcut does same work as above
+  constructor(public name:string, public amount: number){  }
+}
